ad2e83453000eca620acc38de9961d3f986e4ffd
manish kumar <manish.kumar212111@gmail.com>
2019-12-07 00:42:37 +0530

    Logout API added

--- App/configs/constants.js
+++ App/configs/constants.js
@@ -1,9 +1,9 @@
-global.HOST_DB     = 'localhost';
-global.USER_DB     = 'root';
-global.PASSWORD_DB = 'root';
-global.DATABASE_DB = 'pintaki';
-
 // global.HOST_DB     = 'localhost';
-// global.USER_DB     = 'pintakii_pintaki';
-// global.PASSWORD_DB = 'F$mTWp(hQ1Zv';
-// global.DATABASE_DB = 'pintakii_pintaki';
+// global.USER_DB     = 'root';
+// global.PASSWORD_DB = 'root';
+// global.DATABASE_DB = 'pintaki';
+
+global.HOST_DB     = 'localhost';
+global.USER_DB     = 'pintakii_pintaki';
+global.PASSWORD_DB = 'F$mTWp(hQ1Zv';
+global.DATABASE_DB = 'pintakii_pintaki';
